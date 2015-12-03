a0b0edfbbfd32d95433b6c7f47a4bb096b9afc86
Tom Barnes <ruralmyth@gmail.com>
2015-12-03 17:44:52 +0000

    running in circles

--- app/assets/javascript/application.js
+++
@@ -1,7 +0,0 @@
-//= require angular.js
-//= require angular-resource.js
-//= require_tree .
-//= require jquery.js
-//= require app.js
-//= require gitUserSearchController.js
-//= require searchFactory.js
---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,6 @@
+//= require jquery.js
+//= require jquery_ujs
+//= require angular.js
+//= require angular-resource.js
+//= require app.js
+//= require_tree .
