9e465509b0b36c07839205ee17254a551c9eddb9
danwheeler <da-wheeler@hotmail.co.uk>
2015-12-03 11:44:06 +0000

    added bower anbower and js files

--- app/assets/javascript/application.js
+++ app/assets/javascript/application.js
@@ -1,1 +1,7 @@
 //= require_tree .
+ //= require jquery.js
+ //= require angular.js
+ //= require angular-resource.js
+ //= require app.js
+ // = require gitUserSearchController.js
+ // = require searchFactory.js
